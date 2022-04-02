e957b6b1c931c097bac340277b6decf0bada85e3
shere <shere1765@gmail.com>
2022-04-02 12:58:47 +0900

    0.4.0.2

---
+++ myAlgorithm/binary_search.py
@@ -0,0 +1,41 @@
+from typing import Sequence, Any
+
+def binary_search(arr: Sequence, target: Any) -> int:
+    """이진검색 입니다."""
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        center = (low + high) // 2
+        guess = arr[center]
+        
+        if guess == target:
+            return center
+        
+        if center < target:
+            low = center + 1
+            
+        if center > target:
+            high = center - 1
+    
+    return -1
+
+if __name__ == "__main__":
+    num = int(input('배열 원소의 개수를 입력하세요.: '))
+    x = [None] * num
+    
+    x[0] = int(input(f'x[0]: '))
+    
+    for i in range(1, num):
+        while True:
+            x[i] = int(input(f'x[{i}]: '))
+            if x[i] > x[i - 1]:
+                break
+            
+    target = int(input('검색하려는 값을 입력하세요.: '))
+    idx = binary_search(x, target)
+    
+    if idx == -1:
+        print('찾으려는 값이 존재하지 않습니다.')
+    
+    print(f'검색하려는 값({target})이 x[{idx}]에 존재합니다.')    
---
+++ myAlgorithm/linear_search.py
@@ -0,0 +1,32 @@
+from re import L
+from typing import Sequence, Any
+from copy import deepcopy
+
+def linear_search(arr: Sequence, target: Any) -> int:
+    """보초법을 사용한 선형검색입니다."""
+    sentinel = deepcopy(arr)
+    sentinel.append(target)
+    i = 0
+    
+    while True:
+        if arr[i] == target:
+            break
+        i += 1
+        
+    return -1 if i == len(arr) else i
+
+if __name__ == "__main__":
+    num = int(input('배열 원소의 수를 입력하세요.: '))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}] : '))
+        
+    target = int(input('검색하려는 값을 입력하세요.: '))
+    result = linear_search(x, target)
+    
+    if result == -1:
+        print('값을 찾는데 실패하였습니다.')
+    print(f'{target}은 x[{result}]에 위치합니다.')
+    
+    
